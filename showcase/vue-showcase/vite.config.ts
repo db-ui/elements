6e1c5237f622a9363c609b0296c106b51f1e33c7
Duncan Kolba <duncan.kolba@deutschebahn.com>
2022-07-12 14:22:03 +0200

    WIP

--- showcase/vue-showcase/vite.config.ts
+++ showcase/vue-showcase/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-showcase/',
+  base: '',
   build: {
     outDir: 'dist/vue-showcase'
   },
