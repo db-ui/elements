25e8c0c0fc1c065c338a99f2312659fb76d9e17f
Nicolas Merget <104347736+nmerget@users.noreply.github.com>
2022-07-22 11:35:19 +0200

    fix: issue with cicd dependencies (#11)

--- scripts/cleanup-gh-pages.js
+++ scripts/cleanup-gh-pages.js
@@ -1,35 +1,39 @@
 /*
  * Fetches all branches and deletes all review-branches in github pages
  */
-
-import got from 'got';
-import fs from 'fs';
+const fs = require('fs');
+const axios = require('axios');
 
 const TAG = 'cleanup-gh-pages:';
 
-const run = async () => {
+const run = () => {
   if (fs.existsSync('public/review')) {
-    const data = await got(
-      'https://api.github.com/repos/db-ui/elements/branches'
-    ).json();
-    if (data?.length > 0) {
-      const dirsToDelete = fs
-        .readdirSync('public/review')
-        .filter((file) => !data.find((branch) => branch.name === file));
-      if (dirsToDelete?.length > 0) {
-        console.log(TAG, 'Start removing branches from gh-pages');
-        console.log(TAG, dirsToDelete);
-        dirsToDelete.forEach((dir) => {
-          fs.rmSync(`public/review/${dir}`, {
-            recursive: true,
-            force: true
-          });
-          console.log(TAG, `deleted branch ${dir}`);
-        });
-      } else {
-        console.log(TAG, 'All branches are up to date');
-      }
-    }
+    axios
+      .get('https://api.github.com/repos/db-ui/elements/branches')
+      .then((res) => {
+        const data = res.data;
+        if (data?.length > 0) {
+          const dirsToDelete = fs
+            .readdirSync('public/review')
+            .filter((file) => !data.find((branch) => branch.name === file));
+          if (dirsToDelete?.length > 0) {
+            console.log(TAG, 'Start removing branches from gh-pages');
+            console.log(TAG, dirsToDelete);
+            dirsToDelete.forEach((dir) => {
+              fs.rmSync(`public/review/${dir}`, {
+                recursive: true,
+                force: true
+              });
+              console.log(TAG, `deleted branch ${dir}`);
+            });
+          } else {
+            console.log(TAG, 'All branches are up to date');
+          }
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 };
 
