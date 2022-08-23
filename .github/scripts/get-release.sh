64f215985deed11cff3af5ac454a65136643239d
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2022-08-23 15:23:16 +0200

    fix: issue with missing release environment var

--- .github/scripts/get-release.sh
+++ .github/scripts/get-release.sh
@@ -4,12 +4,13 @@ if [[ $GITHUB_REF == refs/tags/v*  ]]
 then
   if [[ $GITHUB_ACTOR != 'dependabot[bot]' ]]
   then
-    if [[ $GITHUB_COMMITISH == 'main' && $GITHUB_RELEASE == true  ]]
-    then
-      echo "RELEASE"
-    elif [[ $GITHUB_PRE_RELEASE == true  ]]
+
+    if [[ $GITHUB_PRE_RELEASE == true  ]]
     then
       echo "PRE_RELEASE"
+    elif [[ $GITHUB_COMMITISH == 'main' ]]
+    then
+      echo "RELEASE"
     fi
    else
      echo "Dependabot has no permission to publish!"
