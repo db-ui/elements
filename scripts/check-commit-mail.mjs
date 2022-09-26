31252e46624f72fd8efc9169e6d3737836ffdb16
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2022-09-26 13:41:45 +0200

    refactor: aligned scripts according to xo/eslint feedback

--- scripts/check-commit-mail.mjs
+++ scripts/check-commit-mail.mjs
@@ -1,15 +1,19 @@
-require('dotenv').config();
+#!/usr/bin/env node
+import * as ChildProcess from 'node:child_process';
+import * as process from 'node:process';
+import * as dotenv from 'dotenv';
 
-const ChildProcess = require('child_process');
+dotenv.config();
 
 const checkCommitMail = () => {
-  console.log('Check COMMIT_MAIL');
+  console.log(`Check COMMIT_MAIL`);
   if (!process.env.COMMIT_MAIL) {
     console.error(
-      "No COMMIT_MAIL set in .env, please look at the file '.env.template'"
+      `No COMMIT_MAIL set in .env, please look at the file '.env.template'`
     );
     process.exit(1);
   }
+
   const currentMail = ChildProcess.execSync('git config user.email')
     .toString()
     .trim()
