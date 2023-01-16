963c6f72568ea6a8c450c6599a3dfb14b21cbb85
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2023-01-16 08:26:12 +0100

    Update imprint.stories.tsx

--- packages/db-ui-elements-stencil/src/legal/imprint.stories.tsx
+++ packages/db-ui-elements-stencil/src/legal/imprint.stories.tsx
@@ -8,7 +8,7 @@ export default {
 };
 
 export const imprint = () => {
-  return `<script type="text/javascript">
+  return `<a href="https://www.dbsystel.de/dbsystel-en/legal-3715942" target="_blank" rel="noopener noreferrer">Imprint</a><script type="text/javascript">
     top.location.href = 'https://www.dbsystel.de/dbsystel-en/legal-3715942';
 </script>`;
 };
