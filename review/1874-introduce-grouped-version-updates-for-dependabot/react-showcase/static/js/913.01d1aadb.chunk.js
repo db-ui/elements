28201eddb7e0129648edee9c18d40b0d164455c2
nmerget <nmerget@users.noreply.github.com>
2023-10-18 00:40:59 +0000

    deploy: 8fe36b0379422ecb460e65e21ce3982e19e1aa3f

--- review/1874-introduce-grouped-version-updates-for-dependabot/react-showcase/static/js/913.01d1aadb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_showcase=self.webpackChunkreact_showcase||[]).push([[913],{2913:function(t,n,e){e.r(n),e.d(n,{db_meta:function(){return s}});var u=e(5671),r=e(3144),o=e(2697),s=function(){function t(n){(0,u.Z)(this,t),(0,o.r)(this,n)}return(0,r.Z)(t,[{key:"render",value:function(){return(0,o.h)(o.H,null,(0,o.h)("slot",null))}}]),t}();s.style=":host{margin-top:auto;margin-bottom:auto}"}}]);
-//# sourceMappingURL=913.01d1aadb.chunk.js.map
