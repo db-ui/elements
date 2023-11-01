a3b7019eb88d892bf64b73bdfe6f378d1050fda4
mfranzke <mfranzke@users.noreply.github.com>
2023-11-01 09:44:56 +0000

    deploy: 1170bc6852d9642ca02908e3d1f4c94bbf080c87

---
+++ review/1964-db-mainnavigation-is-completely-broken-below-1023-px-viewport/angular-lts-minus-1-showcase/700.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkangular_lts_minus_1_showcase=self.webpackChunkangular_lts_minus_1_showcase||[]).push([[700],{700:(o,n,t)=>{t.r(n),t.d(n,{db_meta:()=>a});var s=t(487);const a=class{constructor(l){(0,s.r)(this,l)}render(){return(0,s.h)(s.H,null,(0,s.h)("slot",null))}};a.style=":host{margin-top:auto;margin-bottom:auto}"}}]);
