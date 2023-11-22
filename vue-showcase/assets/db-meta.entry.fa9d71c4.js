f088744ad4566256d3d01fb5b1a0b412777f6e09
mfranzke <mfranzke@users.noreply.github.com>
2023-11-22 09:10:35 +0000

    deploy: b7f9275ab8ced6e68c96c9584fbab5678c64ea0a

--- vue-showcase/assets/db-meta.entry.fa9d71c4.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as t,H as r}from"./index.fe6e36ce.js";const a=":host{margin-top:auto;margin-bottom:auto}",e=class{constructor(s){o(this,s)}render(){return t(r,null,t("slot",null))}};e.style=a;export{e as db_meta};
