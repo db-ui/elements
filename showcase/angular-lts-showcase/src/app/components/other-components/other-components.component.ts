d27f911559136424313108365baca046d0417a2a
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2023-11-26 21:34:13 +0100

    feat: provide angular 15 showcase (#1658)
    * chore: regenerated package-lock.json
    
    * chore: updated angular dependencies
    
    * chore: updated angular dependencies
    
    * chore: corrected the path
    
    * chore: updated angular dependencies
    
    * chore: updated and moved angular dependencies
    
    * chore: added angular specific gitignore aspects
    
    * Update howto-angular.md
    
    * refactor: moved to typed form controls
    
    * chore: regenerated package-lock.json files
    
    * chore: it's unclear what this plugin is all about
    
    * refactor: we need to downgrade typescript for Angular 14
    
    * chore: regenerate package-lock.json
    
    * chore: regenerated lock files
    
    * refactor: regenerated lock file
    
    * chore: regenerated lock files
    
    * chore: regenerated lock files
    
    * chore: regenerated lock file
    
    * chore: regenerated lock file
    
    * chore: regenerated lock files
    
    * chore: regenerated lock files and added postcss
    
    * refactor: regenerated lock files
    
    * chore: regenerated lock files
    
    * chore: regenerated lock files
    
    * chore: regenerated lock files
    
    * refactor: updated lock files
    
    * refactor: extracted lint ignore rules
    
    * refactor: removed deprecated option
    
    https://github.com/ng-packagr/ng-packagr/blob/main/CHANGELOG.md#1300-2021-11-03
    
    * refactor: moved this folder according to its contents
    
    * refactor: updated versions again
    
    * refactor: needed to revert typescript to version < 4.9.0
    
    due to Angular 14
    
    * refactor: needed to adapt those after console messages
    
    * refactor: updated that list
    
    * refactor: corrected that artifact name
    
    * docs: further details
    
    * refactor: that zone.js version is still required by @angular/core@14.3.0
    
    * refactor: updated jest and jest-preset-angular
    
    * refactor: regenerated lock files
    
    * refactor: reverted that update which has been previously done by accident
    
    * chore: another try
    
    * Discard changes to showcase/vue-showcase/package-lock.json
    
    * refactor: further places which we needed to update
    
    * refactor: another try
    
    * chore: another try
    
    * chore: another try
    
    * fix: value
    
    * refactor: regenerated lock files
    
    * chore: another try
    
    * chore: another try
    
    * refactor: regenerated lock files
    
    * refactor: renamed folder
    
    * feat: added Angular 15
    
    * refactor: doing ng update
    
    ng update @angular/core@15 @angular/cli@15
    
    * refactor: regenerated lock files
    
    * refactor: corrected that merge aspect
    
    * Discard changes to packages/db-ui-elements-angular/package.json
    
    * refactor: updated lock files
    
    * refactor: regenerated lock files
    
    * refactor: regenerated package lock file
    
    * refactor: regenerated package lock file

---
+++ showcase/angular-lts-showcase/src/app/components/other-components/other-components.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-components',
+  templateUrl: './other-components.component.html',
+  styleUrls: ['other-components.component.css']
+})
+export class OtherComponentsComponent {
+  notification: boolean;
+}
