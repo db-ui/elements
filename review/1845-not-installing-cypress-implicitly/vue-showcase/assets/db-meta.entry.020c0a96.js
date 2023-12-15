45117de34991f5d66bba2bdb05301f1383426f81
mfranzke <mfranzke@users.noreply.github.com>
2023-12-15 09:39:28 +0000

    deploy: 530358eb21c88e84331f752b991f506abb54acc6

---
+++ review/1845-not-installing-cypress-implicitly/vue-showcase/assets/db-meta.entry.020c0a96.js
@@ -0,0 +1,1 @@
+import{r as o,h as t,H as r}from"./index.ea36bc5e.js";const a=":host{margin-top:auto;margin-bottom:auto}",e=class{constructor(s){o(this,s)}render(){return t(r,null,t("slot",null))}};e.style=a;export{e as db_meta};
