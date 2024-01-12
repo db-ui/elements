3b7cfb4763175846e5326c8809d9d24af8daabe1
mfranzke <mfranzke@users.noreply.github.com>
2024-01-12 13:38:52 +0000

    deploy: 5b84cb65ce1a9059ad421e57b915212b3b0169a4

--- review/1932-form-elements-provide-possibility-to-visually-hide-the-label/vue-showcase/assets/db-meta.entry.0ae65b58.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as t,H as r}from"./index.a6af4fa3.js";const a=":host{margin-top:auto;margin-bottom:auto}",e=class{constructor(s){o(this,s)}render(){return t(r,null,t("slot",null))}};e.style=a;export{e as db_meta};
