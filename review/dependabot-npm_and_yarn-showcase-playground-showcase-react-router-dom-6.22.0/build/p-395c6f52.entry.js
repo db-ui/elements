af38cb23df251dd18919744b667c47d7b16a39eb
nmerget <nmerget@users.noreply.github.com>
2024-02-05 13:35:38 +0000

    deploy: 752b2a4a2b4e1dd7ca903ba5778cdd7b8b64893c

---
+++ review/dependabot-npm_and_yarn-showcase-playground-showcase-react-router-dom-6.22.0/build/p-395c6f52.entry.js
@@ -0,0 +1,2 @@
+import{r as a,h as c,H as t}from"./p-6c189ce1.js";const o=":host{margin-top:auto;margin-bottom:auto}";const s=o;const r=class{constructor(c){a(this,c)}render(){return c(t,{key:"3f17aa991650ca351c4ade9aabeca5afb5db270e"},c("slot",{key:"52289a2f8b0a8adc38786c0f662d685c4cdcbc45"}))}};r.style=s;export{r as db_meta};
+//# sourceMappingURL=p-395c6f52.entry.js.map
