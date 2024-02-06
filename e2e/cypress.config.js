802ddb0809bf952994a183ef4795e5383db7baff
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-02-06 12:53:02 +0100

    chore: enabled videos

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,17 +1,16 @@
 const { defineConfig } = require('cypress');
 const { configureVisualRegression } = require('cypress-visual-regression');
+const { unlinkSync } = require('node:fs');
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  reporter: 'junit',
-  reporterOptions: {
-    mochaFile: './cypress/results/output-[hash].xml'
-  },
   experimentalStudio: true,
   viewportWidth: 1920,
   viewportHeight: 1200,
   screenshotsFolder: './cypress/snapshots/actual',
   trashAssetsBeforeRuns: true,
+  video: true,
+  videoCompression: false,
   env: {
     ALWAYS_GENERATE_DIFF: false
   },
@@ -19,6 +18,18 @@ module.exports = defineConfig({
     screenshotsFolder: './cypress/snapshots/actual',
     setupNodeEvents(on, config) {
       configureVisualRegression(on);
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            unlinkSync(results.video);
+          }
+        }
+      });
     },
     baseUrl: 'http://localhost:6006'
   }
