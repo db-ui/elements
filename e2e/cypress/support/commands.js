1f8a7a7c5e8104857edea057c30aaec76b9168a6
NicolasMerget <Nicolas.Merget@deutschebahn.com>
2024-02-08 12:06:16 +0100

    chore: update snapshots

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -3,22 +3,30 @@ const {
 } = require('cypress-visual-regression/dist/command');
 addCompareSnapshotCommand();
 
-Cypress.Commands.add('snap', (test, threshold = 0.15, overwrite = true) => {
-  // wait till all components are loaded and styles applied
-  cy.wait(600);
-  if (Cypress.browser.isHeadless) {
-    cy.compareSnapshot(test, {
-      overwrite: overwrite,
-      errorThreshold: threshold
-    });
-  } else {
-    // headless mode are doubling components and changing layout we add an extra threshold
-    cy.compareSnapshot(test, {
-      overwrite: overwrite,
-      errorThreshold: threshold + 0.2
-    });
+Cypress.Commands.add(
+  'snap',
+  (test = '', threshold = 0.15, overwrite = true) => {
+    // wait till all components are loaded and styles applied
+    if (test.startsWith('showcase')) {
+      cy.get('db-page').should('exist');
+    } else {
+      cy.get(test).should('exist');
+    }
+    cy.wait(1000);
+    if (Cypress.browser.isHeadless) {
+      cy.compareSnapshot(test, {
+        overwrite: overwrite,
+        errorThreshold: threshold
+      });
+    } else {
+      // headless mode are doubling components and changing layout we add an extra threshold
+      cy.compareSnapshot(test, {
+        overwrite: overwrite,
+        errorThreshold: threshold + 0.2
+      });
+    }
   }
-});
+);
 
 Cypress.Commands.add('gotoStory', (category, component, isIntro = true) => {
   cy.visit(
