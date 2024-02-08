bbb8b5ac2e1ab2ab2c808ae522e37d9e81dbc3b1
nmerget <nmerget@users.noreply.github.com>
2024-02-08 13:55:50 +0000

    deploy: 4fece9e1531bc8d5bd59a402bb429bacb27ca22a

--- review/main/assets/preview-YJcEjSCh.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-9OOhB-uW.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-_Ew3MpVS.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-_Ew3MpVS.js","./chunk-HLWAVYOI-dT3FcS3D.js","./iframe-9OOhB-uW.js","./doctrine-5o-Yg3-5.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
