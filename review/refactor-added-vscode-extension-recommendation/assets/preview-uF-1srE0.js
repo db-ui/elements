743e66d6ad6a508f0380442af0d07bc9d3a371a1
mfranzke <mfranzke@users.noreply.github.com>
2024-02-15 07:34:23 +0000

    deploy: d3ab0a3aedb21a8c1d2552386f7a5d63b8c7ba7a

---
+++ review/refactor-added-vscode-extension-recommendation/assets/preview-uF-1srE0.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-BHfA9EiI.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DEi8MnBg.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DEi8MnBg.js","./chunk-HLWAVYOI-CKE217rf.js","./iframe-BHfA9EiI.js","./index-C-frfP5L.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
