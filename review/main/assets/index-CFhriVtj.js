b5f50c0cb7616e33bda93570d7660cdf93520298
mfranzke <mfranzke@users.noreply.github.com>
2024-02-15 09:06:26 +0000

    deploy: 3becc2ba93bab527d443aa485798a88d5396bc64

--- review/main/assets/index-CFhriVtj.js
+++
@@ -1,1 +0,0 @@
-import{M as e,a,u as n,w as M}from"./index-BCk5NHTx.js";import"./chunk-HLWAVYOI-BaA1M1hb.js";import"./iframe-DufaNlGy.js";import"../sb-preview/runtime.js";import"./index-C-frfP5L.js";import"./index-DrFu-skq.js";export{e as MDXContext,a as MDXProvider,n as useMDXComponents,M as withMDXComponents};
--- review/main/assets/preview-BJw0dOnB.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-DufaNlGy.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-tpoNbC7d.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-tpoNbC7d.js","./chunk-HLWAVYOI-BaA1M1hb.js","./iframe-DufaNlGy.js","./index-C-frfP5L.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
