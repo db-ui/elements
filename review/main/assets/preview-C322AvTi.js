094549667a5f0ea28842409d89bcd1c90fb5ecaf
mfranzke <mfranzke@users.noreply.github.com>
2024-03-14 18:25:38 +0000

    deploy: 324aa6beb0021516bfb45a4dd905aef621f3c767

--- review/main/assets/preview-C322AvTi.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-CpqH_dRj.js","./chunk-HLWAVYOI-DybMae0A.js","./iframe-CWojUjqW.js","./index-C-frfP5L.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as e}from"./iframe-CWojUjqW.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-CpqH_dRj.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{a as parameters};
