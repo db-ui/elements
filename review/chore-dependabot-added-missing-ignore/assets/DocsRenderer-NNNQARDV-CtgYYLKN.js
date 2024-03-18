56665898207c45fd88047747d77d250aa5975218
mfranzke <mfranzke@users.noreply.github.com>
2024-03-18 05:24:25 +0000

    deploy: 195ccd6793feb4bed8916d84017dc4aab01285e0

---
+++ review/chore-dependabot-added-missing-ignore/assets/DocsRenderer-NNNQARDV-CtgYYLKN.js
@@ -0,0 +1,1 @@
+import{D as i,d as s}from"./chunk-HLWAVYOI-DCfNFUHE.js";import"./iframe-CxpyqfHj.js";import"../sb-preview/runtime.js";import"./index-C-frfP5L.js";import"./index-DrFu-skq.js";export{i as DocsRenderer,s as defaultComponents};
---
+++ review/chore-dependabot-added-missing-ignore/assets/showcase-angular-lts-minus-1.stories-Cs9Nm1PO.js
@@ -0,0 +1,7 @@
+/* empty css                 */const n={title:"00-Intro/Showcases",parameters:{layout:"fullscreen"}},s=()=>`<div class="iframe-container">
+      <iframe src="angular-lts-minus-1-showcase/index.html" height="100vh" width="100%" />
+    </div>`;var r,e,a;s.parameters={...s.parameters,docs:{...(r=s.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  return \`<div class="iframe-container">
+      <iframe src="angular-lts-minus-1-showcase/index.html" height="100vh" width="100%" />
+    </div>\`;
+}`,...(a=(e=s.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const i=["Angular_NG_lts_minus_1_build"];export{s as Angular_NG_lts_minus_1_build,i as __namedExportsOrder,n as default};
