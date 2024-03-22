04653c08df929f18b0bbbf3b782e4dd559d400bf
mfranzke <mfranzke@users.noreply.github.com>
2024-03-22 06:56:32 +0000

    deploy: 2d757e470e6632a9ff0e682cb1d445a46b1b19f3

---
+++ review/dependabot-github_actions-dependabot-fetch-metadata-2/assets/showcase-angular-lts.stories-C3MLPmCg.js
@@ -0,0 +1,7 @@
+/* empty css                 */const n={title:"00-Intro/Showcases",parameters:{layout:"fullscreen"}},r=()=>`<div class="iframe-container">
+      <iframe src="angular-lts-showcase/index.html" height="100vh" width="100%" />
+    </div>`;var e,s,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`() => {
+  return \`<div class="iframe-container">
+      <iframe src="angular-lts-showcase/index.html" height="100vh" width="100%" />
+    </div>\`;
+}`,...(a=(s=r.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const i=["Angular_NG_lts_build"];export{r as Angular_NG_lts_build,i as __namedExportsOrder,n as default};
