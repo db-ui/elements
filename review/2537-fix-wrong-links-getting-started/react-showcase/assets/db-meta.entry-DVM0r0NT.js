875eb4c30892951c40e81e8236e1c1489b38b57a
nmerget <nmerget@users.noreply.github.com>
2024-05-23 14:55:52 +0000

    deploy: 4f8a3a153719333af77b9d35e9ad1ac03a97cbf7

---
+++ review/2537-fix-wrong-links-getting-started/react-showcase/assets/db-meta.entry-DVM0r0NT.js
@@ -0,0 +1,1 @@
+import{r as e,h as t,H as s}from"./index-DtCPm6iu.js";const c=":host{margin-top:auto;margin-bottom:auto}",o=c,r=class{constructor(a){e(this,a)}render(){return t(s,{key:"3f17aa991650ca351c4ade9aabeca5afb5db270e"},t("slot",{key:"52289a2f8b0a8adc38786c0f662d685c4cdcbc45"}))}};r.style=o;export{r as db_meta};
