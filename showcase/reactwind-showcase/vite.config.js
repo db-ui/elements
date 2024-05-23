5ee306826089b465f15115c78dd71dde257dd79b
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-05-23 16:44:24 +0200

    fix: issue with react-showcases wrong path

--- showcase/reactwind-showcase/vite.config.js
+++ showcase/reactwind-showcase/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: `/reactwind-showcase/`,
+    base: '',
     build: {
       outDir: 'dist/reactwind-showcase'
     },
