2e9e5bae2996690289b5a157cf85dfb8e82a19f6
mfranzke <mfranzke@users.noreply.github.com>
2024-06-20 15:30:45 +0000

    deploy: 7fadd76c1e80ebca7a7c7dcccb4c41307d39569f

---
+++ review/refactor-codeowners-added-bruno/assets/db-footer.intro.stories-Cgv8hQEd.js
@@ -0,0 +1,25 @@
+import{M as s,C as n,e as i}from"./chunk-HLWAVYOI-3NzLQDU3.js";import{j as e}from"./jsx-runtime-ksGqeha-.js";import{u as d}from"./index-DTTS-Jem.js";import"./iframe-pqs2-D5R.js";import"../sb-preview/runtime.js";import"./doctrine-CNNg9870.js";import"./index-DrFu-skq.js";const c=`# db-footer
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property    | Attribute   | Description                                                                        | Type      | Default |
+| ----------- | ----------- | ---------------------------------------------------------------------------------- | --------- | ------- |
+| \`border\`    | \`border\`    | The border attribute can be set to add a border on top of the footer.              | \`boolean\` | \`false\` |
+| \`copyright\` | \`copyright\` | The copyright attribute can be set to enable a copyright string with current year. | \`boolean\` | \`false\` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
+`;function a(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"02-Navigation/Footer/Intro"}),`
+`,e.jsx("db-headline",{variant:"1",children:"<db-footer>"}),`
+`,e.jsx("db-headline",{variant:"2",children:"Default Footer"}),`
+`,e.jsx(n,{mdxSource:"<db-footer copyright border />",children:e.jsx("db-footer",{copyright:!0,border:!0})}),`
+`,e.jsx("db-headline",{variant:"2",children:"Custom Footer"}),`
+`,e.jsx(n,{mdxSource:'<db-footer>{"Custom Footer"}</db-footer>',children:e.jsx("db-footer",{children:"Custom Footer"})}),`
+`,e.jsx(i,{children:c})]})}function p(t={}){const{wrapper:o}=Object.assign({},d(),t.components);return o?e.jsx(o,{...t,children:e.jsx(a,{...t})}):a()}const l=()=>{throw new Error("Docs-only story")};l.parameters={docsOnly:!0};const r={title:"02-Navigation/Footer/Intro",tags:["stories-mdx"],includeStories:["__page"]};r.parameters=r.parameters||{};r.parameters.docs={...r.parameters.docs||{},page:p};const j=["__page"];export{j as __namedExportsOrder,l as __page,r as default};
