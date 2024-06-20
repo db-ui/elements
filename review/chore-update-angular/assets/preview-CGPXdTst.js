d855184cc3c3d4e8eebc165d577a9a0fa9fd4f67
nmerget <nmerget@users.noreply.github.com>
2024-06-20 11:17:48 +0000

    deploy: 2139d8bac449c13e250be2652f1914a9f16b6280

---
+++ review/chore-update-angular/assets/preview-CGPXdTst.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-DtTmVMet.js","./chunk-HLWAVYOI-3NzLQDU3.js","./iframe-pqs2-D5R.js","./doctrine-CNNg9870.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as e}from"./iframe-pqs2-D5R.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-DtTmVMet.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{a as parameters};
