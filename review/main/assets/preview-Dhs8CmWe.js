b60091e79de5d5c23e3fb05c9f7b042bfa2d5503
mfranzke <mfranzke@users.noreply.github.com>
2024-07-09 07:30:37 +0000

    deploy: 718bf27715c5fb0fee5eb387ed83ca567bf040bd

--- review/main/assets/preview-Dhs8CmWe.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-C11-oRBk.js","./chunk-HLWAVYOI-zrBjNT3i.js","./iframe-dSOcri_E.js","./doctrine-CNNg9870.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
-import{_ as e}from"./iframe-dSOcri_E.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-C11-oRBk.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{a as parameters};
