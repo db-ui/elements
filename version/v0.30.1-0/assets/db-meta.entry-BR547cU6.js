20bf9ada85867fac0bd72e3a1f28d12d5b32029a
mfranzke <mfranzke@users.noreply.github.com>
2024-07-09 07:26:12 +0000

    deploy: 7bca1555625cded6facfba12e47c09f0876605ec

---
+++ version/v0.30.1-0/assets/db-meta.entry-BR547cU6.js
@@ -0,0 +1,1 @@
+import{r as e,h as t,H as o}from"./preview-DI7kkhnP.js";import"./iframe-dSOcri_E.js";import"../sb-preview/runtime.js";const s=":host{margin-top:auto;margin-bottom:auto}",c=s,r=class{constructor(a){e(this,a)}render(){return t(o,{key:"3f17aa991650ca351c4ade9aabeca5afb5db270e"},t("slot",{key:"52289a2f8b0a8adc38786c0f662d685c4cdcbc45"}))}};r.style=c;export{r as db_meta};
