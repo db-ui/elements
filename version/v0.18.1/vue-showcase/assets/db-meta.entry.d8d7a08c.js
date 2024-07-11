6a40c0077e852e2dc0ac35667c50e1ec50ee1d66
nmerget <nmerget@users.noreply.github.com>
2024-07-11 00:46:50 +0000

    deploy: 5a5f1ce16c1797dcd2bb17983c2f8f9cda539742

--- version/v0.18.1/vue-showcase/assets/db-meta.entry.d8d7a08c.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as t,H as r}from"./index.dbcaf92d.js";const a=":host{margin-top:auto;margin-bottom:auto}",e=class{constructor(s){o(this,s)}render(){return t(r,null,t("slot",null))}};e.style=a;export{e as db_meta};
