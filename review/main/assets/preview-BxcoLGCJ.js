89233d463d64ffa7ec030210f804db617762c2cb
mfranzke <mfranzke@users.noreply.github.com>
2024-07-31 08:10:44 +0000

    deploy: 7a0e87f3081410c518ce79046dd318f52dc3f645

---
+++ review/main/assets/preview-BxcoLGCJ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-C-2mWFzZ.js","./chunk-HLWAVYOI-CXESfmt4.js","./iframe-BYcQhOMg.js","./doctrine-CNNg9870.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./iframe-BYcQhOMg.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-C-2mWFzZ.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url);return new r}}};export{a as parameters};
