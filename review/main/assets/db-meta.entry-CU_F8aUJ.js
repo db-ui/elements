32ebc25e4b96785dfe3d3bd84e2be4a32af56734
mfranzke <mfranzke@users.noreply.github.com>
2024-08-05 07:00:51 +0000

    deploy: f2c38dcc6338efdb4075d3a7084be713cfb5bc51

---
+++ review/main/assets/db-meta.entry-CU_F8aUJ.js
@@ -0,0 +1,1 @@
+import{r as o,h as t,H as s}from"./preview-uvkT-jN5.js";import"./iframe-DnHCaCWp.js";import"../sb-preview/runtime.js";const a=":host{margin-top:auto;margin-bottom:auto}",r=a,c=class{constructor(e){o(this,e)}render(){return t(s,{key:"330ff736ae2b915b6c546d554bed1f081198fc09"},t("slot",{key:"5b9868da492482c392c153777671469a92efdc1b"}))}};c.style=r;export{c as db_meta};
