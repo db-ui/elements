f9117bf5932a0d7c43030db5b5e23920746e7d80
nmerget <nmerget@users.noreply.github.com>
2024-08-24 00:46:40 +0000

    deploy: 108574b03876f88ba212287405267d494748983a

--- version/v0.22.0/angular-showcase/0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkangular_showcase=self.webpackChunkangular_showcase||[]).push([[0],[(l,a,t)=>{t.r(a),t.d(a,{db_meta:()=>n});var s=t(677);const n=class{constructor(u){(0,s.r)(this,u)}render(){return(0,s.h)(s.H,null,(0,s.h)("slot",null))}};n.style=":host{margin-top:auto;margin-bottom:auto}"}]]);
