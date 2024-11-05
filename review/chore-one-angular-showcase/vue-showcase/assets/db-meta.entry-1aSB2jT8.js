3ac91484c876c96cd4aaa6061e7d4594f63a658f
nmerget <nmerget@users.noreply.github.com>
2024-11-05 09:37:42 +0000

    deploy: 55972b7ff79267e5566213e4f16bfca85be30c58

---
+++ review/chore-one-angular-showcase/vue-showcase/assets/db-meta.entry-1aSB2jT8.js
@@ -0,0 +1,1 @@
+import{h as e,H as r,r as o}from"./index-CLnC9DVL.js";var n=":host{margin-top:auto;margin-bottom:auto}",s=n,b=function(){function t(a){o(this,a)}return t.prototype.render=function(){return e(r,{key:"330ff736ae2b915b6c546d554bed1f081198fc09"},e("slot",{key:"5b9868da492482c392c153777671469a92efdc1b"}))},t}();b.style=s;export{b as db_meta};
