ee0bb964668087c5e80529567a0595b6694e395b
nmerget <nmerget@users.noreply.github.com>
2025-08-03 01:15:36 +0000

    deploy: 34a1480528c40fcdfeb1df62fe27d5027071ecdc

--- review/refactor-mfranzke-patch-1/assets/showcase-react.stories-ZQz6g3HG.js
+++
@@ -1,7 +0,0 @@
-/* empty css                 */const c={title:"00-Intro/Showcases",parameters:{layout:"fullscreen"}},e=()=>`<div class="iframe-container">
-      <iframe src="react-showcase/index.html" height="100%" width="100%" />
-    </div>`;var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
-  return \`<div class="iframe-container">
-      <iframe src="react-showcase/index.html" height="100%" width="100%" />
-    </div>\`;
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const i=["React_Vite_build"];export{e as React_Vite_build,i as __namedExportsOrder,c as default};
