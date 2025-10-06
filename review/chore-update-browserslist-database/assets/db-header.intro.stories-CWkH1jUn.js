2ca20585d0f883db8f1d2113f7db51a782460e25
mfranzke <mfranzke@users.noreply.github.com>
2025-10-06 05:28:29 +0000

    deploy: efca8e566629dd3801301056267c24ef063a85ad

---
+++ review/chore-update-browserslist-database/assets/db-header.intro.stories-CWkH1jUn.js
@@ -0,0 +1,19 @@
+import{M as s,C as d,e as o}from"./chunk-HLWAVYOI-D_4ZzuQ_.js";import{j as e}from"./jsx-runtime-CWCqKZIK.js";import{u as a}from"./index-H-OnFwWC.js";import"./iframe-CkDCpfKn.js";import"../sb-preview/runtime.js";import"./index-DofHZ8ew.js";import"./index-DrFu-skq.js";const c=`# db-header
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
+`;function i(n){const r=Object.assign({p:"p"},a(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"02-Navigation/Header/Intro"}),`
+`,e.jsx(r.p,{children:"A header component."}),`
+`,e.jsx("p",{children:"Only with brand"}),`
+`,e.jsx(d,{mdxSource:'<db-header><db-brand>{"DB-UI Elements"}</db-brand></db-header>',children:e.jsx("db-header",{children:e.jsx("db-brand",{children:"DB-UI Elements"})})}),`
+`,e.jsx("p",{children:"With navigation"}),`
+`,e.jsx(d,{mdxSource:'<db-header><db-brand>{"DB-UI Elements"}</db-brand><db-mainnavigation><db-link href="#" rel="index" current="page">{"Startseite"}</db-link><db-link href="#" rel="offers">{"Auftr\\xE4ge"}</db-link></db-mainnavigation></db-header>',children:e.jsxs("db-header",{children:[e.jsx("db-brand",{children:"DB-UI Elements"}),e.jsxs("db-mainnavigation",{children:[e.jsx("db-link",{href:"#",rel:"index",current:"page",children:"Startseite"}),e.jsx("db-link",{href:"#",rel:"offers",children:"Aufträge"})]})]})}),`
+`,e.jsx("p",{children:"With meta navigation"}),`
+`,e.jsx(d,{mdxSource:'<db-header><db-brand>{"DB-UI Elements"}</db-brand><db-meta><db-metanavigation><db-link icon="settings" href="#">{"Einstellungen"}</db-link><db-link icon="chat" icononly href="#">{"Nachrichten"}</db-link></db-metanavigation></db-meta></db-header>',children:e.jsxs("db-header",{children:[e.jsx("db-brand",{children:"DB-UI Elements"}),e.jsx("db-meta",{children:e.jsxs("db-metanavigation",{children:[e.jsx("db-link",{icon:"settings",href:"#",children:"Einstellungen"}),e.jsx("db-link",{icon:"chat",icononly:!0,href:"#",children:"Nachrichten"})]})})]})}),`
+`,e.jsx(o,{children:c})]})}function l(n={}){const{wrapper:r}=Object.assign({},a(),n.components);return r?e.jsx(r,{...n,children:e.jsx(i,{...n})}):i(n)}const h=()=>{throw new Error("Docs-only story")};h.parameters={docsOnly:!0};const t={title:"02-Navigation/Header/Intro",tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:l};const f=["__page"];export{f as __namedExportsOrder,h as __page,t as default};
